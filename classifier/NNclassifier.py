0df4803b144b9f25192835b3786d72eb18793978
yaacov <kobi.zamir@gmail.com>
2018-03-18 20:36:08 +0200

    fix linting

--- classifier/NNclassifier.py
+++ classifier/NNclassifier.py
@@ -14,11 +14,11 @@ parser = argparse.ArgumentParser(description='A learning line Classifier.')
 parser.add_argument('image', metavar='FILE', type=str,
                     help='image file name to proccess')
 parser.add_argument('sampels', metavar='N', type=int, nargs='?', default=360,
-                    help='number of positive training points')
+                    help='number of training points')
 args = parser.parse_args()
 
 # get number of sumples from user
-num_of_samples = args.sampels
+num_of_samples_per_category = args.sampels / 2
 
 # get imagename from user
 img_name = args.image
@@ -31,25 +31,44 @@ training_label = []
 
 print("Create some training data.")
 training_length = 0
-
-while training_length < num_of_samples:
+while training_length < num_of_samples_per_category:
     # randomly chose point
     x, y = random.randint(0, width - 33), random.randint(0, length - 33)
 
+    if output_image[x+16, y+16][0] == 0:
+        continue
+
     # append label
-    output_scalar = 0.99 if output_image[x+16, y+16][0] > 0 else 0.01
-    training_label.append(output_scalar)
+    training_label.append(0.01)
 
     # append training data
     input_matrix = input_image[x:(x+32), y:(y+32)]
     training_data.append(input_matrix)
 
-    training_length = training_length + output_scalar
+    training_length = training_length + 1
+
+training_length = 0
+while training_length < num_of_samples_per_category:
+    # randomly chose point
+    x, y = random.randint(0, width - 33), random.randint(0, length - 33)
+
+    if output_image[x+16, y+16][0] != 0:
+        continue
+
+    # append label
+    training_label.append(0.99)
+
+    # append training data
+    input_matrix = input_image[x:(x+32), y:(y+32)]
+    training_data.append(input_matrix)
+
+    training_length = training_length + 1
 
 training_data = np.array(training_data, dtype=np.uint8)
-#training_label = np.array(training_label, dtype=np.uint8)
+training_label = np.array(training_label, dtype=np.uint8)
 print(training_data.shape)
-#print(training_label.shape)
+print(training_label.shape)
+
 
 def createModel():
     model = Sequential()
@@ -81,8 +100,8 @@ print("Start training on data.")
 
 model1 = createModel()
 batch_size = 256
-epochs = 100
+epochs = 13
 model1.compile(optimizer='rmsprop', loss='sparse_categorical_crossentropy',
                metrics=['accuracy'])
 model1.fit(training_data, training_label,
-                    batch_size=batch_size, epochs=epochs, verbose=2)
+           atch_size=batch_size, epochs=epochs, verbose=2)
