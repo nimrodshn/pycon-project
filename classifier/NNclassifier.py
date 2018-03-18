0d1a868c6a4253be7f56c7ce690321c43e47f49a
yaacov <kobi.zamir@gmail.com>
2018-03-18 20:28:18 +0200

    use less samples

--- classifier/NNclassifier.py
+++ classifier/NNclassifier.py
@@ -1,31 +1,60 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
 
 from keras.models import Sequential
 from keras.layers import Dense, Conv2D, MaxPooling2D, Dropout, Flatten
+import argparse
 import cv2
 import numpy as np
+import random
 
-img_name = 'a1.jpg'
+random.seed()
+
+parser = argparse.ArgumentParser(description='A learning line Classifier.')
+parser.add_argument('image', metavar='FILE', type=str,
+                    help='image file name to proccess')
+parser.add_argument('sampels', metavar='N', type=int, nargs='?', default=360,
+                    help='number of positive training points')
+args = parser.parse_args()
+
+# get number of sumples from user
+num_of_samples = args.sampels
+
+# get imagename from user
+img_name = args.image
 input_image = cv2.imread('pictures/' + img_name)
 output_image = cv2.imread('pictures/out/' + img_name)
 
-width,length,ch = input_image.shape
-
+width, length, _ = input_image.shape
 training_data = []
 training_label = []
 
-for x in range(width-32):
-    for y in range(length-32):
-        input_matrix = input_image[x:(x+32),y:(y+32)]
-        output_scalar = 1 if output_image[x+16,y+16][0] > 0 else 0
-        training_data.append(input_matrix)
-        training_label.append(output_scalar)
+print("Create some training data.")
+training_length = 0
 
-print len(training_data)
-print len(training_label)
+while training_length < num_of_samples:
+    # randomly chose point
+    x, y = random.randint(0, width - 33), random.randint(0, length - 33)
+
+    # append label
+    output_scalar = 0.99 if output_image[x+16, y+16][0] > 0 else 0.01
+    training_label.append(output_scalar)
+
+    # append training data
+    input_matrix = input_image[x:(x+32), y:(y+32)]
+    training_data.append(input_matrix)
+
+    training_length = training_length + output_scalar
+
+training_data = np.array(training_data, dtype=np.uint8)
+#training_label = np.array(training_label, dtype=np.uint8)
+print(training_data.shape)
+#print(training_label.shape)
 
 def createModel():
     model = Sequential()
-    model.add(Conv2D(32, (3, 3), padding='same', activation='relu', input_shape=training_data[0].shape))
+    model.add(Conv2D(32, (3, 3), padding='same', activation='relu',
+                     input_shape=training_data[0].shape))
     model.add(Conv2D(32, (3, 3), activation='relu'))
     model.add(MaxPooling2D(pool_size=(2, 2)))
     model.add(Dropout(0.25))
@@ -47,8 +76,13 @@ def createModel():
 
     return model
 
+
+print("Start training on data.")
+
 model1 = createModel()
 batch_size = 256
-epochs = 3
-model1.compile(optimizer='rmsprop', loss='categorical_crossentropy', metrics=['accuracy'])
-model1.fit(training_data, training_label, verbose=2, batch_size=batch_size, epochs=epochs)
+epochs = 100
+model1.compile(optimizer='rmsprop', loss='sparse_categorical_crossentropy',
+               metrics=['accuracy'])
+model1.fit(training_data, training_label,
+                    batch_size=batch_size, epochs=epochs, verbose=2)
--- classifier/classifier.py
+++ classifier/classifier.py
@@ -1,3 +1,6 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
 import argparse
 import cv2
 import numpy as np
@@ -5,42 +8,45 @@ from matplotlib import pyplot as plt
 
 parser = argparse.ArgumentParser(description='A simple text line Classifier.')
 parser.add_argument('image', metavar='FILE', type=str,
-                   help='image file name to proccess')
+                    help='image file name to proccess')
 args = parser.parse_args()
 
+# get imagename from user
 img_name = args.image
 img = cv2.imread('pictures/' + img_name)
 img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-img = cv2.medianBlur(img,5)
+img = cv2.medianBlur(img, 5)
 
 # threshold
-th = cv2.adaptiveThreshold(img,255,cv2.ADAPTIVE_THRESH_GAUSSIAN_C, cv2.THRESH_BINARY,11,2)
+th = cv2.adaptiveThreshold(img, 255, cv2.ADAPTIVE_THRESH_GAUSSIAN_C,
+                           cv2.THRESH_BINARY, 11, 2)
 ddw = th
 
 # invert image (background to 0, letters to 1)
 th = cv2.bitwise_not(th)
 
 # denoising image.
-denoise_kernel = np.ones((3,3),np.uint8)
+denoise_kernel = np.ones((3, 3), np.uint8)
 th = cv2.morphologyEx(th, cv2.MORPH_OPEN, denoise_kernel)
 
 # classifier.
-classify_kernel = np.ones((2,60),np.uint8)
+classify_kernel = np.ones((2, 60), np.uint8)
 th = cv2.morphologyEx(th, cv2.MORPH_CLOSE, classify_kernel)
 th = cv2.morphologyEx(th, cv2.MORPH_OPEN, classify_kernel)
 
 # denoise again
-denoise_kernel = np.ones((10,1),np.uint8)
+denoise_kernel = np.ones((10, 1), np.uint8)
 th = cv2.morphologyEx(th, cv2.MORPH_OPEN, denoise_kernel)
 
 # fixed false negative
-denoise_kernel = np.ones((5,5),np.uint8)
-th = cv2.dilate(th,denoise_kernel,iterations = 1)
+denoise_kernel = np.ones((5, 5), np.uint8)
+th = cv2.dilate(th, denoise_kernel, iterations=1)
 
-img = cv2.merge((ddw,ddw,th))
+# save output image
+img = cv2.merge((th, th, th))
+cv2.imwrite("pictures/out/" + img_name, img)
 
-plt.imshow(img,'gray')
-
-img = cv2.merge((th,th,th))
-cv2.imwrite("pictures/out/" + img_name,img)
+# show output image
+img = cv2.merge((ddw, ddw, th))
+plt.imshow(img, 'gray')
 plt.show()
